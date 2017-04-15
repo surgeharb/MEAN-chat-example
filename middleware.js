4eed640fda8391f8ba405bf5f4106daadcd6546d
surgeharb <me@sergeharb.com>
2017-04-15 16:03:29 +0300

    add API middleware

---
+++ middleware.js
@@ -0,0 +1,30 @@
+const _ = require("underscore");
+const jsonToken = require('./libs/token.js');
+
+module.exports = function(router, config) {
+
+  // route middleware that will happen on every request
+  router.use(function(req, res, next) {
+
+    if (_.contains(config.get("allowedRoutes"), req.method + " " + req.url)) { //routes that do not need any authorization
+      next();
+    } else { //routes related to the callServer only
+      if (typeof(req.headers.authorization) == "undefined") {
+        return res.status(401).json({ message: 'missing authorization header' });
+      } else { //other api routes that needs JWT authorization
+        jsonToken.verify(req.headers.authorization, 'chat')
+          .then(function(response) {
+            if (response.message === 'verified') {
+              next();
+            } else {
+              return res.status(401).json(response.message);
+            }
+          })
+          .catch(function() {
+            return res.status(401).json({ message: 'invalid authorization header' });
+          })
+      }
+    }
+  });
+
+}
